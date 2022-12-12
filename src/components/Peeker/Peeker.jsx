d16e0b9e4d6946d605c2c6c3755dc42ba624ce16
Patrick Grady <patrickrgrady81@gmail.com>
2022-12-12 10:23:34 -0500

    init commit

---
+++ src/components/Peeker/Peeker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Peeker() {
+  return (
+    <div>Peeker</div>
+  )
+}
+
+export default Peeker
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Peeker from './components/Peeker/Peeker';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Peeker />
+  </React.StrictMode>,
+)
